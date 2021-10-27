cf9b6c203ba4f562f199a030ec6baa439cfe504e
Igor Feshchenko <igor777.fesh@mail.ru>
2021-10-27 07:18:32 +0700

    Add new window when push button

---
+++ Meshes/Window1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Shapes;
+using HelixToolkit.Wpf;
+
+namespace Meshes
+{
+    public partial class Window1 : Window
+    {
+        ModelVisual3D device3D = new ModelVisual3D();
+        public Window1(String path)
+        {
+            InitializeComponent();
+            device3D.Content = Display3d(path);
+            viewPort3d.Children.Add(device3D);
+        }
+
+        private Model3D Display3d(String model)
+        {
+            Model3D device = null;
+            try
+            {
+                viewPort3d.RotateGesture = new MouseGesture(MouseAction.LeftClick);
+                ModelImporter import = new ModelImporter();
+                device = import.Load(model);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Exception Error : " + e.StackTrace);
+            }
+            return device;
+        }
+    }
+}
